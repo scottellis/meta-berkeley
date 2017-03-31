7db63d23dd7927404aae38d7a33cfc826e7a1efc
Scott Ellis <scott@jumpnowtek.com>
2017-03-31 06:31:48 -0400

    Use different names for partitions

--- scripts/copy_rootfs.sh
+++ scripts/copy_rootfs.sh
@@ -13,9 +13,9 @@ if [ ! -d /media/card ]; then
 fi
 
 if [ "x${2}" = "x" ]; then
-        IMAGE=console
+	IMAGE=lbl
 else
-        IMAGE=${2}
+	IMAGE=${2}
 fi
 
 if [ -z "$OETMP" ]; then
@@ -35,32 +35,32 @@ fi
 echo "IMAGE: $IMAGE"
 
 if [ "x${3}" = "x" ]; then
-        TARGET_HOSTNAME=$MACHINE
+	TARGET_HOSTNAME=$MACHINE
 else
-        TARGET_HOSTNAME=${3}
+	TARGET_HOSTNAME=${3}
 fi
 
 echo "HOSTNAME: $TARGET_HOSTNAME"
 
 
 if [ ! -f "${SRCDIR}/${IMAGE}-image-${MACHINE}.tar.xz" ]; then
-        echo "File not found: ${SRCDIR}/${IMAGE}-image-${MACHINE}.tar.xz"
-        exit 1
+	echo "File not found: ${SRCDIR}/${IMAGE}-image-${MACHINE}.tar.xz"
+	exit 1
 fi
 
 if [ -b ${1} ]; then
-        DEV=${1}
+	DEV=${1}
 elif [ -b "/dev/${1}2" ]; then
-        DEV=/dev/${1}2
+	DEV=/dev/${1}2
 elif [ -b "/dev/${1}p2" ]; then
-        DEV=/dev/${1}p2
+	DEV=/dev/${1}p2
 else
-        echo "Block device not found: /dev/${1}2 or /dev/${1}p2"
-        exit 1
+	echo "Block device not found: /dev/${1}2 or /dev/${1}p2"
+	exit 1
 fi
 
 echo "Formatting $DEV as ext4"
-sudo mkfs.ext4 -q -L ROOT $DEV
+sudo mkfs.ext4 -q -L rootfs $DEV
 
 echo "Mounting $DEV"
 sudo mount $DEV /media/card
